024158fc8e368b5d529419ed841e1386b5232683
Mina-Emad-20 <167125155+Mina-Emad-20@users.noreply.github.com>
2024-11-22 15:39:27 +0200

    updated

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -4,6 +4,7 @@ import site_three from "/sites/Screenshot 2024-09-07 023918.png";
 import site_four from "/sites/Screenshot 2024-09-07 024026.png";
 import site_five from "/sites/Screenshot 2024-09-07 023949.png";
 import site_six from "/sites/Screenshot 2024-09-07 024105.png";
+import site_seven from "/sites/ecommercePic.png";
 
 export const myProjects = [
     { title: "Furniture Website", description: "Introducing our exclusive collection of premium furniture, meticulously curated for the discerning homeowner seeking to elevate their living spaces", category: "css", imgpath: site_one, link: "https://keen-nougat-0b6620.netlify.app/", github: "https://github.com/Mina-Emad-20/Furniture-Web" },
@@ -35,5 +36,10 @@ export const myProjects = [
 
 
 
-    { title: "E-commerce Website", description: "E-commerce websites have revolutionized the way we shop. Dive into the world of online shopping and learn the ins and outs of setting up and running a successful e-commerce website", category: "react", imgpath: site_six, link: "https://iridescent-lebkuchen-d3beb8.netlify.app/", github: "https://github.com/Mina-Emad-20/Shop.co" }
+    { title: "E-commerce Website", description: "E-commerce websites have revolutionized the way we shop. Dive into the world of online shopping and learn the ins and outs of setting up and running a successful e-commerce website", category: "react", imgpath: site_six, link: "https://iridescent-lebkuchen-d3beb8.netlify.app/", github: "https://github.com/Mina-Emad-20/Shop.co" },
+
+
+
+
+    { title: "Happy-shope E-commerce", description: "E-commerce websites have revolutionized the way we shop. Dive into the world of online shopping and learn the ins and outs of setting up and running a successful e-commerce website", category: "react", imgpath: site_seven, link: "https://spectacular-faun-39e950.netlify.app", github: "https://github.com/Mina-Emad-20/happy-shop-ecommerce" }
 ]
